d640d0c45ec2103f586fbbc6fc2b20f410d10de1
sinmetal <sinmetal@sinmetalcraft.jp>
2024-10-14 20:57:31 +0900

    複数のColumnをAND条件に繋ぐ、ARRAYを追加など、検索条件の複雑にできるような関数を作成

--- service.go
+++ service.go
@@ -1,10 +1,14 @@
 package spanner_demo_20240801
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
+	"fmt"
+	"strings"
+	"text/template"
 	"time"
 
 	"cloud.google.com/go/spanner"
@@ -15,6 +19,8 @@ const SampleMessagesTable = "SampleMessages"
 
 type SampleMessage struct {
 	SampleMessageID string    `json:"sampleMessageID"`
+	Tags            []string  `json:"tags"`
+	Title           string    `json:"title"`
 	Message         string    `json:"message"`
 	CreatedAt       time.Time `json:"createdAt"`
 }
@@ -88,6 +94,105 @@ func (s *Service) SearchMessage(ctx context.Context, text string) ([]*SearchMess
 			return nil, err
 		}
 		rets = append(rets, &v)
+	}
+	return rets, nil
+}
+
+type SearchSampleMessagesReq struct {
+	Tag     string `json:"tag"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
+type SearchSampleMessagesResp struct {
+	SampleMessageID string    `json:"sampleMessageID"`
+	Tags            []string  `json:"tags"`
+	Title           string    `json:"title"`
+	Message         string    `json:"message"`
+	CreatedAt       time.Time `json:"createdAt"`
+}
+
+func (s *Service) SearchSampleMessages(ctx context.Context, req *SearchSampleMessagesReq) ([]*SearchSampleMessagesResp, error) {
+	const sql = `SELECT SampleMessageID, Tags, Title, Message, CreatedAt
+ FROM SampleMessages
+ WHERE {{.WHERE}}
+ LIMIT 50
+`
+	params := make(map[string]interface{})
+	var whereClause []string
+	if req.Tag != "" {
+		whereClause = append(whereClause, "SEARCH(SampleMessages_Tags_Tokens, @tag)")
+		params["tag"] = req.Tag
+	}
+	if req.Title != "" {
+		whereClause = append(whereClause, "SEARCH(SampleMessages_Title_Tokens, @title)")
+		params["title"] = req.Title
+	}
+	if req.Message != "" {
+		whereClause = append(whereClause, "SEARCH(SampleMessages_Message_Tokens, @message)")
+		params["message"] = req.Message
+	}
+	tmpl := template.New("mytemplate")
+	tmpl, err := tmpl.Parse(sql)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	data := map[string]string{"WHERE": strings.Join(whereClause, "AND")}
+	err = tmpl.Execute(&buf, data)
+	if err != nil {
+		return nil, err
+	}
+
+	finishSQL := buf.String()
+	fmt.Println(finishSQL)
+	sts := spanner.NewStatement(finishSQL)
+	sts.Params = params
+	var rets []*SearchSampleMessagesResp
+	iter := s.spa.Single().Query(ctx, sts)
+	for {
+		row, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		v := &SearchSampleMessagesResp{}
+		var sampleMessageID string
+		if err := row.ColumnByName("SampleMessageID", &sampleMessageID); err != nil {
+			return nil, fmt.Errorf("failed get sampleMessageID column : %w", err)
+		}
+		v.SampleMessageID = sampleMessageID
+
+		var tags []string
+		if err := row.ColumnByName("Tags", &tags); err != nil {
+			return nil, fmt.Errorf("failed get Tags column : %w", err)
+		}
+		v.Tags = tags
+
+		var title *string
+		if err := row.ColumnByName("Title", &title); err != nil {
+			return nil, fmt.Errorf("failed get Title column : %w", err)
+		}
+		if title != nil {
+			v.Title = *title
+		}
+
+		var message string
+		if err := row.ColumnByName("Message", &message); err != nil {
+			return nil, fmt.Errorf("failed get message column : %w", err)
+		}
+		v.Message = message
+
+		var createdAt time.Time
+		if err := row.ColumnByName("CreatedAt", &createdAt); err != nil {
+			return nil, fmt.Errorf("failed get CreatedAt column : %w", err)
+		}
+		v.CreatedAt = createdAt
+
+		rets = append(rets, v)
 	}
 	return rets, nil
 }
