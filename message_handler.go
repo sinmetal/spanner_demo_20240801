25ce81fe087505f65b44e27918d8096565330d7a
sinmetal <sinmetal@sinmetalcraft.jp>
2024-10-16 09:43:58 +0900

    title, tagで検索できるようにした

--- message_handler.go
+++ message_handler.go
@@ -11,7 +11,9 @@ type MessageHandler struct {
 }
 
 type PostMessageReq struct {
-	Text string `json:"text"`
+	Title   string   `json:"title"`
+	Message string   `json:"message"`
+	Tags    []string `json:"tags"`
 }
 
 type PostMessageResp struct {
@@ -29,7 +31,9 @@ func (h *MessageHandler) PostMessageHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	msg, err := h.s.Insert(ctx, &SampleMessage{
-		Message: body.Text,
+		Title:   body.Title,
+		Message: body.Message,
+		Tags:    body.Tags,
 	})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -75,6 +79,47 @@ func (h *MessageHandler) SearchHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/javascript;charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	resp := SearchResp{
+		Results: msgs,
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		fmt.Println(err)
+	}
+}
+
+type SearchCompositeReq struct {
+	Tag     string `json:"tag"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
+type SearchCompositeResp struct {
+	Results []*SearchSampleMessagesResp `json:"results"`
+}
+
+func (h *MessageHandler) SearchCompositeHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var body SearchCompositeReq
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Printf("failed request body json decode. %s\n", err)
+		return
+	}
+
+	msgs, err := h.s.SearchSampleMessages(ctx, &SearchSampleMessagesReq{
+		Tag:     body.Tag,
+		Title:   body.Title,
+		Message: body.Message,
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Printf("failed SampleMessage.SearchMessage. %s\n", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/javascript;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	resp := SearchCompositeResp{
 		Results: msgs,
 	}
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
